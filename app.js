0bd526d7d165886e81b8ad782796062329312963
Mak Alamin <makalamin012@gmail.com>
2022-02-22 22:41:43 +0600

    loaded cases by country

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
+// Load Countries
 fetch("https://api.covid19api.com/countries")
   .then((res) => res.json())
-  .then((data) => loadCountries(data));
+  .then((data) => {
+    console.log(data);
+    loadCountries(data);
+  });
 
-function loadCountries(data) {
-  let countries = data.map((country) => country.Country);
+function loadCountries(countries) {
   let countrySelect = document.getElementById("countrySelect");
+
   countries.forEach((country) => {
     let option = document.createElement("option");
-    option.innerText = country;
+    option.innerText = country.Country;
+    option.value = country.Slug;
     countrySelect.appendChild(option);
   });
+}
+
+// Load Cases by Country
+function loadCasesByCountry(country) {
+  console.log(country);
+  fetch(`https://api.covid19api.com/total/country/${country}`)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+    });
 }
